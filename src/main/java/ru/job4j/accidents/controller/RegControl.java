f4a2bcf1d0934b5a0091e463e22a8eb0a1f9b919
Admin <Serger9393@mail.ru>
2023-12-13 04:50:29 +0300

    2. Регистрация пользователя [#296069] Fix 1.3

--- src/main/java/ru/job4j/accidents/controller/RegControl.java
+++ src/main/java/ru/job4j/accidents/controller/RegControl.java
@@ -3,6 +3,7 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.AuthorityRepository;
@@ -17,11 +18,14 @@ public class RegControl {
     private final AuthorityRepository authorities;
 
     @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
+    public String regSave(@ModelAttribute User user, Model model) {
         user.setEnabled(true);
         user.setPassword(encoder.encode(user.getPassword()));
         user.setAuthority(authorities.findByAuthority("ROLE_USER"));
-        simpleUserService.save(user);
+        if (simpleUserService.save(user).isEmpty()) {
+            model.addAttribute("message", "Пользователь уже зарегистрирован");
+            return "errors/404";
+        }
         return "redirect:/login";
     }
 
