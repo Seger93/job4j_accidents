e3f28df41e4b439cec677504018730724e6bb62b
Admin <Serger9393@mail.ru>
2023-12-17 20:21:31 +0300

    0. Spring test [#6881] fix update test

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
@@ -22,6 +23,7 @@ import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.service.SimpleAccidentsService;
 
+import java.util.Optional;
 import java.util.Set;
 
 @SpringBootTest(classes = Main.class)
@@ -54,14 +56,15 @@ class AccidentControllerTest {
                 .andExpect(model().attributeExists("types", "rules"));
     }
 
-    @Disabled
     @Test
     @WithMockUser
     @Transactional
     public void whenGetUpdate() throws Exception {
         var accident = new Accident(1, "name1", "description1", "address1",
                 new AccidentType(), Set.of(new Rule()));
-        accidentsService.save(accident, Set.of(1));
+
+        when(accidentsService.findById(accident.getId())).thenReturn(Optional.of(accident));
+
         this.mockMvc.perform(get("/accident/updateAccident").param("id", String.valueOf(accident.getId())))
                 .andExpect(status().isOk())
                 .andExpect(view().name("accident/updateAccident"))
