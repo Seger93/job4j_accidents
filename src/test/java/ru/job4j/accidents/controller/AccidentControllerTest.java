dd88adb5f9d6c5e8873895a880c5cf113bec7de5
Admin <Serger9393@mail.ru>
2023-12-18 03:30:46 +0300

    1. Spring mock [#2096]

--- src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
+++ src/test/java/ru/job4j/accidents/controller/AccidentControllerTest.java
@@ -1,14 +1,18 @@
 package ru.job4j.accidents.controller;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.mockito.Mockito.verify;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -17,12 +21,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import ru.job4j.accidents.Main;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.model.AccidentType;
 import ru.job4j.accidents.model.Rule;
 import ru.job4j.accidents.service.SimpleAccidentsService;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
@@ -54,6 +61,35 @@ class AccidentControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("accident/createAccident"))
                 .andExpect(model().attributeExists("types", "rules"));
+    }
+
+    @Test
+    @WithMockUser
+    public void whenSaveAndUpdateAccident() throws Exception {
+        var accident = new Accident(1, "name1", "description1", "address1",
+                new AccidentType(1, "Type1"), new HashSet<>());
+        Set<Integer> rIds = new HashSet<>();
+        rIds.add(1);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("name", accident.getName());
+        params.add("text", accident.getText());
+        params.add("address", accident.getAddress());
+        params.add("type_id", String.valueOf(accident.getType().getId()));
+        params.add("rIds", "1");
+
+        when(accidentsService.save(accident, rIds)).thenReturn(accident);
+
+        mockMvc.perform(post("/accident/saveAccident")
+                        .params(params))
+                        .andDo(print())
+                        .andExpect(status().is3xxRedirection())
+                        .andExpect(redirectedUrl("/accident"));
+        ArgumentCaptor<Accident> argument = ArgumentCaptor.forClass(Accident.class);
+        ArgumentCaptor<HashSet<Integer>> argumentSet = ArgumentCaptor.forClass(HashSet.class);
+        verify(accidentsService).save(argument.capture(), argumentSet.capture());
+        assertThat(argument.getValue().getName()).isEqualTo("name1");
+        assertThat(argument.getValue().getAddress()).isEqualTo("address1");
+        assertThat(argument.getValue().getText()).isEqualTo("description1");
     }
 
     @Test
--- src/test/java/ru/job4j/accidents/controller/RegControlTest.java
+++ src/test/java/ru/job4j/accidents/controller/RegControlTest.java
@@ -1,10 +1,20 @@
 package ru.job4j.accidents.controller;
 
+import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,6 +25,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.accidents.Main;
+import ru.job4j.accidents.model.Authority;
+import ru.job4j.accidents.model.User;
+import ru.job4j.accidents.service.SimpleUserService;
+
+import java.util.Optional;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
@@ -24,6 +39,9 @@ class RegControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private SimpleUserService userService;
+
     @Test
     @Transactional
     @WithMockUser
@@ -32,5 +50,26 @@ class RegControlTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("/reg"));
+    }
+
+    @Test
+    @Transactional
+    @WithMockUser
+    public void whenPostSaveNewUser() throws Exception {
+        String password = "test";
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(password);
+        User user = new User(0, hashedPassword, "test", new Authority(), true);
+        when(userService.save(any(User.class))).thenReturn(Optional.of(user));
+        mockMvc.perform(post("/reg")
+                        .param("username", user.getUsername())
+                        .param("password", password))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+
+        ArgumentCaptor<User> userCaptor = ArgumentCaptor.forClass(User.class);
+        verify(userService).save(userCaptor.capture());
+        assertThat(userCaptor.getValue().getUsername()).isEqualTo("test");
+        assertThat(encoder.matches(password, userCaptor.getValue().getPassword())).isTrue();
     }
 }
